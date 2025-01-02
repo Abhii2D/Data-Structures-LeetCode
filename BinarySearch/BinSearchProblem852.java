41efe9ba8c1e7c21a147e1ffb34c690e30614ec7
Abhijeet Dhekane <119113356+Abhii2D@users.noreply.github.com>
2025-01-02 13:50:14 +0530

    Add files via upload

---
+++ BinarySearch/BinSearchProblem852.java
@@ -0,0 +1,27 @@
+package Searching.BinarySearch;
+
+public class BinSearchProblem852 {
+
+
+static int MountainPeakElement(int[] arr){
+    int start = 0;
+    int end = arr.length-1;
+
+    while (start<end){
+        int mid = start + (end - start)/2;
+        if(arr[mid] > arr[mid + 1]){
+            end = mid;
+        }else {
+            start = mid + 1;
+        }
+
+    }
+    return start;
+}
+
+    public static void main(String[] args) {
+        int [] arr = {0,1,2,3,4,5,4,3,1};
+        System.out.println(MountainPeakElement(arr));
+    }
+
+}
